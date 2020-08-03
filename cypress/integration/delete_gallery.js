c4b860b0a1905328c70132940de12eee3570ad7e
Tammy-R <ruzictam@gmail.com>
2020-08-03 21:51:48 +0200

    Rute i requestovi

---
+++ cypress/integration/delete_gallery.js
@@ -0,0 +1,51 @@
+import { EMAIL } from '../fixtures/constants.js'
+import { galleryPage } from '../page_object/gallery.page';
+
+describe('Brisanje galerija', () => {
+
+    beforeEach(() => {
+        cy.loginBe(EMAIL.EXISTING, EMAIL.PASSWORD)
+    })
+
+
+    beforeEach(() => {
+        cy.server()
+        cy.route('GET', Cypress.env('apiUrl') + '/my-galleries?page=1&term=').as('galerija')
+
+    })
+
+
+    it('Create ten galleries', () => {
+        cy.visit('/create')
+        for (var i = 1; i <= 10; i++) {
+            galleryPage.create('title', '', 'https://image.shutterstock.com/image-photo/butterfly-grass-on-meadow-night-260nw-1111729556.jpg')
+            cy.visit('/create')
+        }
+
+    })
+
+    it.only('Brisanje', () => {
+        cy.visit('/my-galleries')
+        cy.wait('@galerija')
+        cy.get('@galerija').
+            its('response').then((resp) => {
+                cy.log(resp)
+                for (var i = 0; i <= 10; i++) {  // i mora da bude <= od broja galerija koji su na stranici, inace puca test
+                    let useCaseID = resp.body.galleries[i].id
+                    cy.request({
+                        method: 'DELETE',
+                        url: Cypress.env('apiUrl') + '/galleries/' + resp.body.galleries[i].id,
+                        form: true,
+                        followRedirect: true,
+                        headers: {
+                            authorization: `Bearer ${window.localStorage.getItem('token')}`
+                        },
+                    })
+                }
+            }
+            )
+
+
+    })
+
+})
---
+++ cypress/integration/routeall.js
@@ -0,0 +1,43 @@
+import {EMAIL} from '../fixtures/constants.js'
+
+
+//presreli smo request i poslali podatke iz jsona
+
+describe('Route all', () => {
+
+    beforeEach(() => {
+        cy.server()
+        cy.route('GET', Cypress.env('apiUrl') + '/galleries?page=1&term=').as('stubing')
+    })
+
+    it('Wait for request to load', () => {
+        //request >> umesto da koristimo postman to mozemo da testiramo i u cypressu
+        cy.loginBe(EMAIL.EXISTING, EMAIL.PASSWORD)
+
+        //sve ovo ispod smo spakovali u novu cy.komandu
+        // cy.request('POST', Cypress.env('apiUrl') + '/auth/login',
+        //     { "email": "ruzictam@gmail.com", "password": "0637379360" }).then((resp) => {
+        //         expect(resp.body).to.have.property('access_token')
+        //         localStorage.setItem('token', resp.body.acces_token)
+             
+        //     })
+        // cy.visit('/')
+
+        cy.wait('@stubing')
+        cy.get('@stubing').
+        its('response').then((resp)=>{
+            // cy.log(resp.body.galleries[0].id)
+            //brisanje galerije iz bekenda
+            cy.request({
+                method: 'DELETE',
+                url: Cypress.env('apiUrl') + '/galleries/' + resp.body.galleries[0].id,
+                form: true,
+                followRedirect: true,
+                headers: {
+                    authorization: `Bearer ${window.localStorage.getItem('token')}`
+                },
+            })
+        })
+    })
+
+})
